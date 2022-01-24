dd2d8e0fd198a52c7838b3d0317b386ba503aefd
Valentin349 <valentino7007@gmail.com>
2022-01-24 22:18:24 +0000

    added price comparisons

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import requests
+import requests, time
 
 listURL = "https://api.coingecko.com/api/v3/coins/list"
 pricesURL = "https://api.coingecko.com/api/v3/simple/price"
+coins = []
 
 def getData(url, payload=None):
     try:
@@ -11,20 +12,40 @@ def getData(url, payload=None):
     except requests.exceptions.RequestException as e:
         raise SystemExit(e)
 
-# get all coins
-idStrings = ['']
-ids = []
-counter = 1
-pages = 0
-for data in getData(listURL):
-    if counter > 450:
-        counter = 0
-        pages += 1
-        idStrings.append('')
-    idStrings[pages] += data["id"] + ","
-    ids.append(data['id'])
-    counter += 1
+def getPrices(idStrings, pages):
+    coins.append({})
+    for i in range(pages):
+        data = getData(pricesURL, {'vs_currencies': 'usd', 'include_last_updated_at': 'true', 'ids':idStrings[i]})
+        coins[-1].update(data)
 
-coins = dict.fromkeys(ids, [])
-# get prices
-print(len(coins))
+def getCoinNames():
+    idStrings = ['']
+    ids = []
+    counter = 1
+    pages = 0
+
+    for data in getData(listURL):
+        if counter > 450:
+            counter = 0
+            pages += 1
+            idStrings.append('')
+        idStrings[pages] += data["id"] + ","
+        ids.append(data['id'])
+        counter += 1
+    priceDiff = dict.fromkeys(ids, 0)
+    return idStrings, pages, priceDiff
+
+def main():
+    idStrings, pages, priceDiff = getCoinNames()
+    getPrices(idStrings, pages)
+    time.sleep(10)
+    getPrices(idStrings, pages)
+    while True:
+        for coin in coins[-1]:
+            if "usd" in coins[-1][coin].keys():
+                priceDiff[coin] = coins[-1][coin]['usd'] - coins[-2][coin]['usd']
+        print("bitcoin", priceDiff['bitcoin'])
+        time.sleep(300)
+        getPrices(idStrings, pages)
+
+main()
