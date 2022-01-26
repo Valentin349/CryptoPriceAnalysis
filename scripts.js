513f5cbe00360bfd393526de959928bd52fb1718
Valentin349 <valentino7007@gmail.com>
2022-01-26 23:02:37 +0000

    added getPrices method

--- scripts.js
+++ scripts.js
@@ -1,36 +1,52 @@
-const listURL = "https://api.coingecko.com/api/v3/coins/list"
-const pricesURL = "https://api.coingecko.com/api/v3/simple/price"
-var coins = []
+const listURL = new URL("https://api.coingecko.com/api/v3/coins/list");
+const pricesURL = new URL("https://api.coingecko.com/api/v3/simple/price");
+var coins = {};
 
-async function getData(url, parameters="") {
+
+
+async function getData(url, parameters={}) {
     try{
-    const response = await fetch(url.concat(parameters));
-    return await response.json();
+        const response = await fetch(url);
+        return await response.json();
     } catch(error) {
         console.error(error);
     }
 }
 
-function getCoinNames(){
+function getCoins(){
     var counter = 0;
-    var pages = 0;
     var idStrings = [''];
-    var ids = [];
+    var ids = []
+    var pages = 0;
 
     getData(listURL).then(data =>{
         for (var coin in data){
             if (counter > 450){
                 counter = 0;
                 pages++;
-                idStrings.push("");
+                idStrings.push('');
             }
             idStrings[pages] = String(idStrings[pages]).concat(data[coin].id,',');
-            ids.push(coin.id);
+            ids.push(data[coin].id);
             counter++;
         }
-        // create price diff object bellow
+        ids.forEach(function(v){
+            coins[v] = [];
+        });
+
+        getPrices(idStrings);
     })
-    
+    console.log(pages);
 }
 
-getCoinNames();
+function getPrices(idStrings){
+    const time = Date.now();
+    console.log(idStrings[0]);
+    idStrings.forEach(page => {
+        getData(pricesURL, {'vs_currencies': 'usd', 'include_last_updated_at': 'true', 'ids':page}).then(data => {
+
+        })
+    })
+}
+
+getCoins();
